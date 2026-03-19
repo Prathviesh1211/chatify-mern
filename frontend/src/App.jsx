cadceecdaf897c43189d7be728cfbf0ee4e7c2a2
Prathviesh <prathvieshnaik@gmail.com>
2026-03-20 03:31:51 +0530

    Setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen relative flex items-center justify-center p-4 overflow-hidden bg-zinc-900">
+      {/* subtle premium gradient */}
+      <div className="absolute inset-0 bg-gradient-to-br from-zinc-900 via-zinc-950 to-black" />
+      {/* grid */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#ffffff08_1px,transparent_1px),linear-gradient(to_bottom,#ffffff08_1px,transparent_1px)] bg-[size:14px_24px]" />
+      {/* glow */}
+      <div className="absolute top-0 -left-4 size-96 bg-fuchsia-600 opacity-20 blur-[120px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-400 opacity-20 blur-[120px]" />
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/singup" element={<SignupPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
