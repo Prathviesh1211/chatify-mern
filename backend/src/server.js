e3f2eb18d055974401b8a01a1fb3593ab5bdbf11
Prathviesh <prathvieshnaik@gmail.com>
2026-04-03 03:37:40 +0530

    fix cross-origin cookies

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,14 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // use ENV, not hardcoded
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+); // use ENV, not hardcoded
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
