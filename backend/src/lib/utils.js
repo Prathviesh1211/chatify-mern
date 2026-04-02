78555b21e05e479287ae4fa0e80a64a709ea8e2f
Prathviesh <prathvieshnaik@gmail.com>
2026-04-03 03:26:59 +0530

    Fixed base-url issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken=async(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,//miliseconds
         httpOnly:true, //prevent xss attack :cross-site scripting
-        sameSite:"lax",  //crsf attacks
+        sameSite:"none",  //crsf attacks
         secure:process.env.NODE_ENV==="development"?false:true,
 
     });
